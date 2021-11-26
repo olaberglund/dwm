1a51e22a8f63298aeee09fa30ac39c79aaa4f51c
olaberglund <ola.kj.berglund@gmail.com>
2021-11-26 08:02:54 +0100

    scratchpad patch applied

--- config.def.h
+++ config.def.h
@@ -61,6 +61,8 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char scratchpadname[] = "scratchpad";
+static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34", NULL };
 static const char *browser[] 	= { "brave", NULL };
 static const char *lightup[] 	= { "xbacklight", "-inc", "5", NULL };
 static const char *lightdn[]  = { "xbacklight", "-dec", "5", NULL };
@@ -96,6 +98,7 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_b,      spawn,					 {.v = blueman } },
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,             					XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_section,togglescratch,  {.v = scratchpadcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
